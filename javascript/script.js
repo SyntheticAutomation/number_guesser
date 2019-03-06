37930f99f9d9e1412e5fdb27f5f2399f9e2810d8
SyntheticAutomation <syntheticauto900@gmail.com>
2019-03-06 16:03:02 -0700

    First commit with base code

---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+function DisplayGuess() {
+    var show_number = document.getElementById('guess_field').value;
+    document.getElementById('number-result').innerHTML = show_number;
+}
+
+function ClearInput() {
+    document.getElementById("guess_field").value = "";
+}
