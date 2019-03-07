2b69b9d06e47bba5d2c906bee7320d6372791f37
SyntheticAutomation <syntheticauto900@gmail.com>
2019-03-06 22:57:59 -0700

    Adds the dreaded min/max input functionality.

--- javascript/script.js
+++ javascript/script.js
@@ -3,7 +3,14 @@ $(document).ready(function() {
         $('#clear-button').prop('disabled', this.value == "" ? true : false);
         $('#reset-button').prop('disabled', this.value == "" ? true : false);
     })
+    $('#minimum_field, #maximum_field').keyup(function () {
+        $('#range-button').prop('disabled', this.value == "" ? true : false);
+    })
 });
+
+var random_number;
+var min_value;
+var max_value;
 
 function DisplayGuess() {
     var number = document.getElementById('guess_field').value;
@@ -18,9 +25,6 @@ function ClearInput() {
     document.getElementById("guess_field").value = "";
 }
 
-// creates a random_number on page load so that when someone presses guess button the random number stays the same
-var random_number = Math.floor(Math.random() * 100)
-
 function DisplayEvaluation() {
     var guessed_number = Number(document.getElementById('guess_field').value);
     var output = 'BOOM!';
@@ -33,21 +37,35 @@ function DisplayEvaluation() {
     document.getElementById("evaluation").innerHTML = output;
 }
 
-function ValidateInput() {
+function ValidateInput(min_value, max_value) {
     var input = parseInt(document.getElementById("guess_field").value);
-    if (input < 1 || input > 100)
-    alert("Error: Number must be between 1-100.");
+    if (input < min_value || input > max_value)
+    alert("Error: Number must be in your specified range.");
     return;
 }
 
 function DisplayResponse() {
     var input = parseInt(document.getElementById("guess_field").value);
-    if (input > 1 && input < 100) {
+    if (input > min_value - 1 && input < max_value + 1) {
         DisplayGuessText(); 
         DisplayGuess(); 
         DisplayEvaluation();
     }
     else {
-        ValidateInput();
+        ValidateInput(min_value, max_value);
     }
+}
+
+function RespondtoRange() {
+    min_value = document.getElementById('minimum_field').value
+    max_value = document.getElementById('maximum_field').value
+    // creates a random_number on range button click so that when someone presses guess button,
+    //  the random number stays the same and can be passed to all functions.
+    random_number = Math.floor(Math.random() * max_value)
+    var output = (`Random Number Generated. Guess what it is, from ${Number(min_value)} to ${Number(max_value)}`);
+    if (min_value > max_value) {
+        output = "Error: Your range is invalid. Please try again.";
+    }
+    
+    document.getElementById("range-text").innerHTML = output;
 }
